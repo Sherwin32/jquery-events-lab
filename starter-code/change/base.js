ffe559966fb1a41703a5cc4fef120fe61ece79c3
Sherwin32 <sherwin.shann@gmail.com>
2017-09-28 18:06:50 -0700

    completed the 'change' challenge

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+	$("input.add").on("change", function total(event){
+		var left = parseInt($("input#left").val()) || 0;
+		var right = parseInt($("input#right").val()) || 0;
+		$("input#total").val(left+right);
+	})
   $("#time").text( Date.now() );
 
 })
