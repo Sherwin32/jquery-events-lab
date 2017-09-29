c2ab4802086548cd19491f7bb0c5ee69331e1c90
Sherwin32 <sherwin.shann@gmail.com>
2017-09-28 18:24:10 -0700

    completed bonus task for 'change'

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,11 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
+var resetButton = "<button class=\"reset\">RESET</button>";
+
 $(document).ready(function(){
+	$("body").append(resetButton);
+	$("input#total").on("mousedown mouseup click keyup keydown change", function doNothing(event){
+		event.preventDefault();
+	});
 	$("input.add").on("change", function total(event){
 		var left = parseInt($("input#left").val()) || 0;
 		var right = parseInt($("input#right").val()) || 0;
 		$("input#total").val(left+right);
-	})
+	});
   $("#time").text( Date.now() );
+
+  $(".reset").on("click", function resetHandler(event){
+  	$("input").val("");
+  });
 
 })
